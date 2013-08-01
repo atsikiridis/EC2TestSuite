7e48f52909a7c07a0922838d797690f626bfb9ae
Artem Tsikiridis <atsik@dmst.aueb.gr>
2013-08-01 17:49:38 +0200

    Delete OpenStack_EC2.py

--- OpenStack_EC2.py
+++
@@ -1,7 +0,0 @@
-from EC2_Flavor import EC2_Flavor
-
-class OpenStack_EC2(EC2_Flavor):
-
-    # No real changes for now!
-    def httpreq_runInstances(self):
-        return super(OpenStack_EC2, self).httpreq_runInstances()
